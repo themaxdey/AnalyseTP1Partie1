56e73b7d1f804ae0719de7e1d8428c01f34092ea
themaxdey <39136244+themaxdey@users.noreply.github.com>
2019-01-28 14:13:46 -0500

    Creation du skelette pour l'application

---
+++ src/CV.java
@@ -0,0 +1,19 @@
+
+public class CV {
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int experience;
+	private String[] competences;
+	private String attentes;
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+	
+	
+	
+}
