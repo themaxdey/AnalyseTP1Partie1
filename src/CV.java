940a51a34c4261df51300f3c632c1a326ae96f95
themaxdey <39136244+themaxdey@users.noreply.github.com>
2019-01-28 14:16:08 -0500

    Creation de la classe affiche

--- src/CV.java
+++ src/CV.java
@@ -12,6 +12,29 @@ public class CV {
 	public static void main(String[] args) {
 		
 	}
+	
+	public void affiche() {
+		System.out.println("Nom : " + nom);
+		System.out.println("Prenom : " + prenom);
+		System.out.println("Formation : " + formation);
+		System.out.println("Expériences de travail : " + experience);
+		
+		System.out.print("Compétences : ");
+		for (int i = 0; i < competences.length; i++) {
+			String string = competences[i];	
+			
+			if (i == competences.length - 1) {
+				System.out.print(string + " ");
+			} else {
+				System.out.print(string + ", ");
+			}
+			
+			
+		}
+		
+		System.out.println("\nAttentes vis à vis le cours 4B4 : " + attentes);
+		
+	}
 
 	
 	
